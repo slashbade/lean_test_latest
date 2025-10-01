cc87bbdbd1513c07e0876c3e4af25b92e18297a7
slashbade <slashbladevitali@gmail.com>
2025-10-01 18:04:32 +0800

    change manifest

--- LeanTestLatest/Test.lean
+++ LeanTestLatest/Test.lean
@@ -1,11 +1,12 @@
 import Mathlib
 import Reap
+import Reap.Tactic.Search
 
 open Finset Set
 
 example (s t : Finset ℕ) (h : s ⊆ t)
   (h' : t ⊆ s) : #s = #t := by
-  reap
+  exact le_antisymm (Finset.card_le_card h) (Finset.card_le_card h')
 
 section exercises
 variable {G H : Type*} [Group G] [Group H]
@@ -18,17 +19,23 @@ example (φ : G →* H) (S T : Subgroup H)
 
 example (φ : G →* H) (S T : Subgroup G)
 (hST : S ≤ T) : map φ S ≤ map φ T := by
-  repeat reap
+  simp only [SetLike.le_def, Set.mem_image, SetLike.mem_coe]
+  intro x a
+  simp_all only [Subgroup.mem_map]
+  obtain ⟨w, h⟩ := a
+  obtain ⟨left, right⟩ := h
+  subst right
+  exact ⟨w, hST left, rfl⟩
 
 /--
 Addictive group of $\mathbb{Q}$ is not cyclic.
 -/
 theorem Rat.not_isAddCyclic : ¬ (IsAddCyclic ℚ) := by
-  sorry
+  reap!!
 
 open Real
 
-example : (cos x + sin x)^2 = 2 * sin x * cos x + 1 := by
-  ring_nf
-  rw [← cos_sq_add_sin_sq x]
+example (x : ℝ) : (cos x + sin x)^2 = 2 * sin x * cos x + 1 := by
+  rw [add_sq, sin_sq, cos_sq]
+  simp_all only [one_div]
   ring
